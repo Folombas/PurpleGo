006017dca226e6ad8e36979bc0428257923cc7ea
Folombas <folombas@yandex.ru>
2025-04-24 23:13:54 +0300

    String formatting

--- go-demo/main.go
+++ go-demo/main.go
@@ -6,8 +6,14 @@ import (
 )
 
 func main() {
-	var userHeight = 1.8
-	var userKg float64 = 100
-	var IMT = userKg / math.Pow(userHeight, 2)
-	fmt.Print(IMT)
+	const IMTPower = 2
+	var userHeight float64 
+	var userKg float64
+	fmt.Println("__ Калькулятор степени ожирения __ ")
+	fmt.Print("Введите свой рост в сантиметрах: ")
+	fmt.Scan(&userHeight)
+	fmt.Print("Введите свой вес: ")
+	fmt.Scan(&userKg)
+	IMT := userKg / math.Pow(userHeight / 100, IMTPower)
+	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
 }

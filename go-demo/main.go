cca0a2d4c4588a7da0682686f6c418105c7cb559
Folombas <folombas@yandex.ru>
2025-04-24 23:26:01 +0300

    Creating formatted strings

--- go-demo/main.go
+++ go-demo/main.go
@@ -9,11 +9,12 @@ func main() {
 	const IMTPower = 2
 	var userHeight float64 
 	var userKg float64
-	fmt.Println("__ Калькулятор степени ожирения __ ")
+	fmt.Printf("__ Калькулятор степени ожирения __")
 	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userKg)
 	IMT := userKg / math.Pow(userHeight / 100, IMTPower)
-	fmt.Printf("Ваш индекс массы тела: %.0f", IMT)
+	result := fmt.Sprint("Ваш индекс массы тела: %.0f", IMT)
+	fmt.Print(result)
 }

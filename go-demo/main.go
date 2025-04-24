5f532890260c5ba52bbe5fac33790010a52482bb
Folombas <folombas@yandex.ru>
2025-04-24 23:41:26 +0300

    Creating a function

--- go-demo/main.go
+++ go-demo/main.go
@@ -9,12 +9,16 @@ func main() {
 	const IMTPower = 2
 	var userHeight float64 
 	var userKg float64
-	fmt.Printf("__ Калькулятор степени ожирения __")
+	fmt.Println("__ Калькулятор степени ожирения __")
 	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userKg)
 	IMT := userKg / math.Pow(userHeight / 100, IMTPower)
-	result := fmt.Sprint("Ваш индекс массы тела: %.0f", IMT)
+	outputResult(IMT)
+}
+
+func outputResult(imt float64) {
+	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
 	fmt.Print(result)
 }

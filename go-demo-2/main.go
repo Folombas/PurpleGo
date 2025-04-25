7ea1121a52847c681f44579886eb7d7bd51cf3d4
Folombas <folombas@yandex.ru>
2025-04-26 00:54:46 +0300

    Working with arrays

--- go-demo-2/main.go
+++ go-demo-2/main.go
@@ -1,6 +1,12 @@
-package main 
+package main
+
+import "fmt"
 
 func main() {
-	transactions := [3]int{5, 10, -7}
-	banks = [2]string{"T-Bank", "Alfa-Bank"}
+	transactions := [3]int{1, 2, 3}
+	banks := [2]string{}
+
+	fmt.Println(transactions[1])
+	banks[0] = "T-Bank"
+	fmt.Println(banks)
 }

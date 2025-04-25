72ba2e406feaafd697725252c894afa4ba16c827
Folombas <folombas@yandex.ru>
2025-04-25 22:08:42 +0300

    if else

--- go-demo/main.go
+++ go-demo/main.go
@@ -11,15 +11,23 @@ func main() {
 	fmt.Println("__ Калькулятор степени ожирения __")
 	userKg, userHeight := getUserInput()
 	IMT := calculateIMT(userKg, userHeight)
-	if IMT < 16 {
-		fmt.Println("У вас недостаток веса")
-	}
 	outputResult(IMT)
+	if IMT < 16 {
+		fmt.Println("У вас сильный дефицит массы тела")
+	} else if IMT < 18.5 {
+		fmt.Println("У ваc дефицит массы тела")
+	} else if IMT < 25 {
+		fmt.Println("У ваc нормальный вес")
+	} else if IMT < 30 {
+		fmt.Println("У ваc избыточный вес")
+	} else {
+		fmt.Println("У ваc степень ожирения")
+	}
 }
 
 func outputResult(imt float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
-	fmt.Print(result)
+	fmt.Println(result)
 }
 
 func calculateIMT(userKg float64, userHeight float64) float64 {

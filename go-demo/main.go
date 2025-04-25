75d4a7f59d4b853f5f88c47bd82165c772264d6d
Folombas <folombas@yandex.ru>
2025-04-25 21:18:15 +0300

    return value

--- go-demo/main.go
+++ go-demo/main.go
@@ -6,19 +6,25 @@ import (
 )
 
 func main() {
-	const IMTPower = 2
-	var userHeight float64 
+
+	var userHeight float64
 	var userKg float64
 	fmt.Println("__ Калькулятор степени ожирения __")
 	fmt.Print("Введите свой рост в сантиметрах: ")
 	fmt.Scan(&userHeight)
 	fmt.Print("Введите свой вес: ")
 	fmt.Scan(&userKg)
-	IMT := userKg / math.Pow(userHeight / 100, IMTPower)
+	IMT := calculateIMT(userKg, userHeight)
 	outputResult(IMT)
 }
 
 func outputResult(imt float64) {
 	result := fmt.Sprintf("Ваш индекс массы тела: %.0f", imt)
 	fmt.Print(result)
+}
+
+func calculateIMT(userKg float64, userHeight float64) float64 {
+	const IMTPower = 2
+	IMT := userKg / math.Pow(userHeight/100, IMTPower)
+	return IMT
 }

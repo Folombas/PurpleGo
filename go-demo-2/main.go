555076af1a7e5207abf2da84235e7f7496df5ee8
Folombas <folombas@yandex.ru>
2025-04-26 22:46:15 +0300

    slice example

--- go-demo-2/main.go
+++ go-demo-2/main.go
@@ -3,10 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	transactions := [3]int{1, 2, 3}
+	transactions := [5]int{1, 2, 3, 4, 5}
 	banks := [2]string{}
 
 	fmt.Println(transactions[1])
 	banks[0] = "T-Bank"
 	fmt.Println(banks)
+	patrial := transactions[1:]
+	fmt.Println(patrial)
 }

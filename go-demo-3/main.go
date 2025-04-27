c4373101a516600f1d527614758dc2d212007d2f
Folombas <folombas@yandex.ru>
2025-04-27 22:24:19 +0300

    change map

--- go-demo-3/main.go
+++ go-demo-3/main.go
@@ -4,7 +4,17 @@ import "fmt"
 
 func main() {
 	m := map[string]string{
-		"Google": "https://google.com",
+		"Google": "google.com",
 	}
+	fmt.Println(m)
+	fmt.Println(m["Google"])
+	m["Google"] = "https://google.com"
+	fmt.Println(m)
+	m["Yandex"] = "https://yandex.ru"
+	m["Rutube"] = "https://rutube.ru"
+	fmt.Println(m)
+	delete(m, "Yandex")
+	delete(m, "Y")
+	fmt.Println(m["Y"])
 	fmt.Println(m)
 }

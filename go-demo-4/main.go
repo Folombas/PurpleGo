b54716c86754f893802afe80f8614bc8f0ace909
Folombas <folombas@yandex.ru>
2025-04-29 22:00:00 +0300

    transfer of structures

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -13,28 +13,23 @@ func main() {
 	password := promptData("Введите Ваш пароль")
 	url := promptData("Введите URL")
 
-	// account1 := account{
-	// 	password,
-	// 	"",
-	// 	url,
-	// }
-	account1 := account{
+	myAccount := account{
 		password: password,
 		url:      url,
-		login,
+		login:	login,
 	}
 
-	outputPassword(login, password, url)
-
+	outputPassword(myAccount)
 }
 
 func promptData(prompt string) string {
-	fmt.Print(prompt)
+	fmt.Print(prompt + ": ")
 	var res string
 	fmt.Scan(&res)
 	return res
 }
 
-func outputPassword(login, password, url string) {
-	fmt.Println(login, password, url)
+func outputPassword(acc account) {
+	fmt.Println(acc)
+	fmt.Println(acc.login, acc.password, acc.url)
 }

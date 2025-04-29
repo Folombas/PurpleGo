c60a8144eab404e839ca451239ad9dd64d73e184
Folombas <folombas@yandex.ru>
2025-04-29 23:23:00 +0300

    exercise transfer generation

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -25,16 +25,24 @@ func (acc *account) generatePassword(n int) {
 	acc.password = string(res)
 }
 
+// 2. Если нет пароля, то генерим
 func newAccount(login, password, urlString string) (*account, error) {
+	if login == "" {
+		return nil, errors.New("INVALID_LOGIN")
+	}
 	_, err := url.ParseRequestURI(urlString)
 	if err != nil {
 		return nil, errors.New("INVALID_URL")
 	}
-	return &account {
+	newAcc := &account {
 		url: urlString,
 		login: login,
 		password: password,
-	}, nil
+	}
+	if password == "" {
+		newAcc.generatePassword(12)
+	}
+	return newAcc, nil
 }
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
@@ -45,10 +53,9 @@ func main() {
 	url := promptData("Введите URL")
 	myAccount, err := newAccount(login, password, url)
 	if err != nil {
-		fmt.Println("Неверный формат URL")
+		fmt.Println("Неверный формат URL или Логин")
 		return
 	}
-	myAccount.generatePassword(12)
 	myAccount.outputPassword()
 	fmt.Println(myAccount)
 }
@@ -56,7 +63,7 @@ func main() {
 func promptData(prompt string) string {
 	fmt.Print(prompt + ": ")
 	var res string
-	fmt.Scan(&res)
+	fmt.Scanln(&res)
 	return res
 }
 

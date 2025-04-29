a086b1b87bb899fba7fffa40d0a4edc6bec5d6d9
Folombas <folombas@yandex.ru>
2025-04-29 21:51:45 +0300

    create instance

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -2,12 +2,30 @@ package main
 
 import "fmt"
 
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
 func main() {
 	login := promptData("Введите Ваш логин")
 	password := promptData("Введите Ваш пароль")
 	url := promptData("Введите URL")
 
+	// account1 := account{
+	// 	password,
+	// 	"",
+	// 	url,
+	// }
+	account1 := account{
+		password: password,
+		url:      url,
+		login,
+	}
+
 	outputPassword(login, password, url)
+
 }
 
 func promptData(prompt string) string {

c3aa754ff0370ab8bdc8c49d0f4e52da1e595c5a
Folombas <folombas@yandex.ru>
2025-04-29 22:49:57 +0300

    Exercise - exercise

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -11,25 +11,33 @@ type account struct {
 	url      string
 }
 
-func (acc account) outputPassword() {
+func (acc *account) outputPassword() {
 	fmt.Println(acc)
 	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]	
+	}
+	acc.password = string(res)
 }
 
 var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
 
 func main() {
-	fmt.Println(generatePassword(12))
 	login := promptData("Введите Ваш логин")
-	password := promptData("Введите Ваш пароль")
+	// password := promptData("Введите Ваш пароль")
 	url := promptData("Введите URL")
 
 	myAccount := account{
-		password: password,
-		url:      url,
+		url:    url,
 		login:	login,
 	}
+	myAccount.generatePassword(12)
 	myAccount.outputPassword()
+	fmt.Println(myAccount)
 
 }
 
@@ -41,10 +49,3 @@ func promptData(prompt string) string {
 }
 
 
-func generatePassword(n int) string{
-	res := make([]rune, n)
-	for i := range res {
-		res[i] = letterRunes[rand.IntN(len(letterRunes))]	
-	}
-	return string(res)
-}

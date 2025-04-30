c50ab18f888fc8bf88921c436e03aed3f5209b47
Folombas <folombas@yandex.ru>
2025-04-30 07:40:00 +0300

    packages: code splitting

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -1,57 +1,14 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/rand/v2"
-	"net/url"
 )
-
-type account struct {
-	login    string
-	password string
-	url      string
-}
-
-func (acc *account) outputPassword() {
-	fmt.Println(acc.login, acc.password, acc.url)
-}
-
-func (acc *account) generatePassword(n int) {
-	res := make([]rune, n)
-	for i := range res {
-		res[i] = letterRunes[rand.IntN(len(letterRunes))]	
-	}
-	acc.password = string(res)
-}
-
-// 2. Если нет пароля, то генерим
-func newAccount(login, password, urlString string) (*account, error) {
-	if login == "" {
-		return nil, errors.New("INVALID_LOGIN")
-	}
-	_, err := url.ParseRequestURI(urlString)
-	if err != nil {
-		return nil, errors.New("INVALID_URL")
-	}
-	newAcc := &account {
-		url: urlString,
-		login: login,
-		password: password,
-	}
-	if password == "" {
-		newAcc.generatePassword(12)
-	}
-	return newAcc, nil
-}
-
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
 
 func main() {
 	login := promptData("Введите Ваш логин")
 	password := promptData("Введите Ваш пароль")
 	url := promptData("Введите URL")
-	myAccount, err := newAccount(login, password, url)
+	myAccount, err := newAccountWithTimeStamp(login, password, url)
 	if err != nil {
 		fmt.Println("Неверный формат URL или Логин")
 		return

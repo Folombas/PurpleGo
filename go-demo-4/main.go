29d37d068bf61b3ce7fdfb48500b1a5d2f133f9e
Folombas <folombas@yandex.ru>
2025-05-01 23:11:01 +0300

    exercise: menu selection

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -7,12 +7,49 @@ import (
 )
 
 func main() {
-	createAccount()
+	// 1. Создать аккаунт
+	// 2. Найти аккаунт
+	// 3. Удалить аккаунт
+	// 4. Выход
+	fmt.Println("__Менеджер паролей__")
+Menu:
+	for {
+		variant := getMenu()
+		switch variant {
+		case 1:
+			createAccount()
+		case 2:
+			findAccount()
+		case 3:
+			deleteAccount()
+		default:
+			break Menu
+		}
+	}
+}
+
+func getMenu() int {
+	var variant int
+	fmt.Println("Выберите вариант:")
+	fmt.Println("1. Создать аккаунт")
+	fmt.Println("2. Найти аккаунт")
+	fmt.Println("3. Удалить аккаунт")
+	fmt.Println("4. Выход")
+	fmt.Scan(&variant)
+	return variant
+}
+
+func findAccount() {
+
+}
+
+func deleteAccount() {
+
 }
 
 func createAccount() {
-	login := promptData("Введите Ваш логин")
-	password := promptData("Введите Ваш пароль")
+	login := promptData("Введите логин")
+	password := promptData("Введите пароль")
 	url := promptData("Введите URL")
 	myAccount, err := account.NewAccount(login, password, url)
 	if err != nil {
